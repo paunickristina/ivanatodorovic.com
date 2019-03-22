7b60f7448dc60d9d3397d12092b67ab83a827b80
Kristina Paunic <paunickristina@gmail.com>
2019-03-22 09:53:25 +0100

    news

---
+++ components/News/NewsItem.vue
@@ -0,0 +1,187 @@
+<template>
+	<article class="b-news-item">
+		<div class="b-news-item__wrapper c-border">
+			<h2 class="b-news-item__title">{{ data.title }}</h2>
+			<div class="b-news-item__img">
+				<img src="~assets/img/news/news-2.jpg" alt="">
+			</div>
+			<div class="b-news-item__body">
+				<h2>{{ data.title }}</h2>
+				<p class="b-news-item__desc">{{ data.description }}</p>
+				<p class="b-news-item__date">{{ data.date }} — {{ data.year }}</p>
+			</div>
+		</div>
+	</article>
+</template>
+
+<script>
+export default {
+	props: ["data"]
+}
+</script>
+
+<style scoped lang="scss">
+@import '~assets/scss/base/_base.scss';
+.b-news-item {
+	&__wrapper {
+		// display: grid;
+		position: relative;
+		padding: 0 0 5.3rem 0;
+		margin-bottom: 5.6rem;
+		@include breakpoint(overPhone){
+			display: grid;
+			padding: 0 0 5.8rem 0;
+			grid-column-gap: 10.6%;
+			grid-template-columns: 49% 40%;
+			align-items: center;
+		}
+	}
+	& h2 {
+		@include font-size(30, 30);
+		@include line-height(34, 35);
+		letter-spacing: 0;
+		margin-bottom: 1.6rem;
+		@include breakpoint(overPhone){
+			margin-bottom: 0;
+		}
+	}
+	&__title {
+		@include breakpoint(overPhone){
+			display: none;
+		}
+	}
+	&__img {
+		margin-bottom: 1.9rem;
+		@include breakpoint(overPhone){
+			margin-bottom: 0;
+			// grid-row: 1 / span 10;
+		}
+		& img {
+			width: 100%;
+		}
+	}
+	&__body {
+		// display: grid;
+		@include breakpoint(overPhone){
+			// margin-top: 7rem;
+			// display: flex;
+			// padding: 5rem 0;
+			// flex-wrap: wrap;
+			// align-items: center;
+		}
+		& h2 {
+			display: none;
+			// @include font-size(30, 30);
+			// @include line-height(34, 35);
+			// letter-spacing: 0;
+			// order: 1;
+			// margin-bottom: 1.6rem;
+			// grid-row-start: 1;
+			@include breakpoint(overPhone){
+				display: block;
+				margin-bottom: 4.9rem;
+				// margin-top: 10.4rem;
+				// order: 2;
+				// grid-column-start: 2;
+				// grid-row: 3;
+				// margin-top: 4rem;
+				// grid-row-start: 1;
+				// margin: 10.5rem 0 0;
+			}
+		}
+	}
+	
+	&__desc {
+		@include font-size(14, 14);
+		@include line-height(22, 22);
+		letter-spacing: 0;
+		margin-bottom: 2.1rem;
+		color: $color-gray;
+		// order: 2;
+		// order: 2;
+		@include breakpoint(overPhone){
+			margin-bottom: 4.8rem;
+			padding-right: 10%;
+			// grid-column-start: 2;
+			// grid-row: 5;
+			// grid-row-start: 1;
+			// margin: 18.9rem 5rem 0 0;
+		}
+	}
+	&__date {
+		@include font-size(16, 16);
+		letter-spacing: 0.05em;
+		// order: 2;
+		@include breakpoint(overPhone){
+			// grid-column-start: 2;
+			// grid-row: 7;
+			// margin-top: 2rem;
+			// grid-row-start: 1;
+			// margin-top: 28rem;
+		}
+	}
+
+	// &__wrapper {
+	// 	display: grid;
+	// 	position: relative;
+	// 	padding: 0 0 5.3rem 0;
+	// 	@include breakpoint(overPhone){
+	// 		padding: 0 0 5.8rem 0;
+	// 		grid-column-gap: 10.6%;
+	// 		grid-template-columns: 49% 40%;
+	// 	}
+	// }
+	// &__img {
+	// 	order: 2;
+	// 	margin-bottom: 1.9rem;
+	// 	@include breakpoint(overPhone){
+	// 		margin-bottom: 0;
+	// 		grid-row: 1 / span 10;
+	// 	}
+	// 	& img {
+	// 		width: 100%;
+	// 	}
+	// }
+	// & h2 {
+	// 	@include font-size(30, 30);
+	// 	@include line-height(34, 35);
+	// 	letter-spacing: 0;
+	// 	order: 1;
+	// 	margin-bottom: 1.6rem;
+	// 	@include breakpoint(overPhone){
+	// 		order: 2;
+	// 		grid-column-start: 2;
+	// 		grid-row: 3;
+	// 		margin-top: 4rem;
+	// 		// grid-row-start: 1;
+	// 		// margin: 10.5rem 0 0;
+	// 	}
+	// }
+	// &__desc {
+	// 	@include font-size(14, 14);
+	// 	@include line-height(22, 22);
+	// 	letter-spacing: 0;
+	// 	margin-bottom: 2.1rem;
+	// 	color: $color-gray;
+	// 	order: 2;
+	// 	@include breakpoint(overPhone){
+	// 		grid-column-start: 2;
+	// 		grid-row: 5;
+	// 		// grid-row-start: 1;
+	// 		// margin: 18.9rem 5rem 0 0;
+	// 	}
+	// }
+	// &__date {
+	// 	@include font-size(16, 16);
+	// 	letter-spacing: 0.05em;
+	// 	order: 2;
+	// 	@include breakpoint(overPhone){
+	// 		grid-column-start: 2;
+	// 		grid-row: 7;
+	// 		// margin-top: 2rem;
+	// 		// grid-row-start: 1;
+	// 		// margin-top: 28rem;
+	// 	}
+	// }
+}
+</style>
--- pages/news/_id/index.vue
+++ pages/news/_id/index.vue
@@ -3,11 +3,17 @@
 		<section class="p-news l">
 			<p class="p-news__desc">News</p>
 			<div class="p-news__item" v-for="(item, index) in activeNews" :key="index">
+				<NewsItem :data="item" />
+			</div>
+			<div class="p-news__item" v-for="(item, index) in inactiveNews" :key="index">
+				<NewsItem :data="item" />
+			</div>
+			<!-- <div class="p-news__item" v-for="(item, index) in activeNews" :key="index">
 				<ArticleItem :data="item" />
 			</div>
 			<div class="p-news__item" v-for="(item, index) in inactiveNews" :key="index">
 				<ArticleItem :data="item" />
-			</div>
+			</div> -->
 		</section>
 		<LecturerBlock />
 	</main>
@@ -15,10 +21,12 @@
 
 <script>
 import ArticleItem from "~/components/News/ArticleItem.vue"
+import NewsItem from "~/components/News/NewsItem.vue"
 import LecturerBlock from "~/components/Lecturer/Block.vue"
 export default {
 	components: {
     ArticleItem,
+    NewsItem,
 		LecturerBlock
   },
 	computed: {
@@ -39,7 +47,7 @@ export default {
 				{
 					id: "1",
 					title: "First short fiction film",
-					imgSrc: "~assets/img/news/news-1.jpg",
+					imgSrc: "~assets/img/news/news-2.jpg",
 					date: "January 12",
 					year: "2018",
 					description: "With a great crew, we shot a film about a girl brave enough to face her uncle who sexualy abused her when she was a child."
@@ -47,7 +55,7 @@ export default {
 				{
 					id: "2",
 					title: "Selection committee for German International Ethnographic film festival",
-					imgSrc: "~assets/img/news/news-1.jpg",
+					imgSrc: "~assets/img/news/news-2.jpg",
 					date: "January 12",
 					year: "2018",
 					description: "In this beautful house for five days, we watched approximately 500 films and chose the best ones for the upcoming German International Ethnographic Film Festival, held in Koblenc from May 13-19th."
@@ -55,7 +63,7 @@ export default {
 				{
 					id: "3",
 					title: "Master Class at the Serbian film center",
-					imgSrc: "~assets/img/news/news-1.jpg",
+					imgSrc: "~assets/img/news/news-2.jpg",
 					date: "January 12",
 					year: "2018",
 					description: "I held a master class with think DOKSRBIJA, a weekend workshop."
@@ -63,7 +71,7 @@ export default {
 				{
 					id: "4",
 					title: "Two 7 day workshopin kulturwissenschaft an der uni Koblenc — Germany",
-					imgSrc: "~assets/img/news/news-1.jpg",
+					imgSrc: "~assets/img/news/news-2.jpg",
 					date: "January 12",
 					year: "2018",
 					description: "It was a pleasure working with 13 students to make their own short documentary films."
@@ -71,7 +79,7 @@ export default {
 				{
 					id: "5",
 					title: "A night of films at an alternative cinema with the film maestro, Zelimir Zilnik and myself",
-					imgSrc: "~assets/img/news/news-1.jpg",
+					imgSrc: "~assets/img/news/news-2.jpg",
 					date: "January 12",
 					year: "2018",
 					description: "It was a pleasure and an honor to be alongside Zelimir Zilnik and discuss our films together."
@@ -79,7 +87,7 @@ export default {
 				{
 					id: "6",
 					title: "Fixer for the documentary film “Speak so I can hear you” by Marija Stojnic",
-					imgSrc: "~assets/img/news/news-1.jpg",
+					imgSrc: "~assets/img/news/news-2.jpg",
 					date: "January 12",
 					year: "2018",
 					description: "“Speak so I can hear you“ is a film about Radio Beograd, oldest radio station in Belgrade and its listeners."
@@ -87,7 +95,7 @@ export default {
 				{
 					id: "7",
 					title: "Selection committee for the documentary film projects for the Serbian film center",
-					imgSrc: "~assets/img/news/news-1.jpg",
+					imgSrc: "~assets/img/news/news-2.jpg",
 					date: "January 12",
 					year: "2018",
 					description: "With my four colleges, we were chose the best autour documentary films to get funding by Serbian Film Center grant."
@@ -95,7 +103,7 @@ export default {
 				{
 					id: "8",
 					title: "The film of Ivana Todorovic: a retrospective",
-					imgSrc: "~assets/img/news/news-1.jpg",
+					imgSrc: "~assets/img/news/news-2.jpg",
 					date: "January 12",
 					year: "2018",
 					description: "In a old theatre called “Dorcolsko Narodno Pozoriste” in Belgrade, the films “When I was a Boy, I was a girl”, “Rapresent” and “Everyday life of Roma Children from Blok 71” were screened in front of a audience. With Q&A after."
@@ -103,7 +111,7 @@ export default {
 				{
 					id: "9",
 					title: "Creative mentorship program",
-					imgSrc: "~assets/img/news/news-1.jpg",
+					imgSrc: "~assets/img/news/news-2.jpg",
 					date: "January 12",
 					year: "2018",
 					description: "I’m part of a new generation of The Creative Mentorship program in Belgrade."
@@ -111,7 +119,7 @@ export default {
 				{
 					id: "10",
 					title: "In editing room with new film “Adem’s Island”",
-					imgSrc: "~assets/img/news/news-1.jpg",
+					imgSrc: "~assets/img/news/news-2.jpg",
 					date: "January 12",
 					year: "2018",
 					description: "With Marija Kovacina, an editor, in the editing process of the new short documentary “Adems Island.”"
@@ -119,7 +127,7 @@ export default {
 				{
 					id: "11",
 					title: "Shooting the film “Adem’s Island”",
-					imgSrc: "~assets/img/news/news-1.jpg",
+					imgSrc: "~assets/img/news/news-2.jpg",
 					date: "January 12",
 					year: "2018",
 					description: "With my team, Masa Drndic DP and Radisa Cvetkovic sound."
@@ -127,7 +135,7 @@ export default {
 				{
 					id: "12",
 					title: "Selection committee for Roma film archive in Budampest",
-					imgSrc: "~assets/img/news/news-1.jpg",
+					imgSrc: "~assets/img/news/news-2.jpg",
 					date: "January 12",
 					year: "2018",
 					description: "I was chosen to be a member of the selection committee to choose films about Roma for the first ever Roma Film Archive."
@@ -135,7 +143,7 @@ export default {
 				{
 					id: "13",
 					title: "A new curated series featuring short documentary films funded in part through Kickstarter",
-					imgSrc: "~assets/img/news/news-1.jpg",
+					imgSrc: "~assets/img/news/news-2.jpg",
 					date: "January 12",
 					year: "2018",
 					description: "I was chosen to be a member of the selection committee to choose films about Roma for the first ever Roma Film Archive."
@@ -149,14 +157,15 @@ export default {
 <style scoped lang="scss">
 @import '~assets/scss/base/_base.scss';
 .p-news {
-	padding-top: 11.6rem;
+	padding-top: 10.8rem;
 	@include breakpoint(overPhone){
-		padding-top: 17.4rem;
+		padding-top: 15.7rem;
 	}
 	&__desc {
-		@include font-size(16, 16);
+		@include font-size(22, 30);
 		letter-spacing: 0.05em;
 		font-style: italic;
+		color: $color-red;
 		text-transform: capitalize;
 		margin-bottom: 6.4rem;
 	}
